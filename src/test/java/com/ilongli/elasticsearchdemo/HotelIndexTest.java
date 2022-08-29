98f69cfde7b351d1297d4eaa456c2717e748fe5f
ilongli <351365415@qq.com>
2022-08-29 17:09:50 +0800

    add: test

--- src/test/java/com/ilongli/elasticsearchdemo/HotelIndexTest.java
+++ src/test/java/com/ilongli/elasticsearchdemo/HotelIndexTest.java
@@ -2,6 +2,12 @@ package com.ilongli.elasticsearchdemo;
 
 import org.apache.http.HttpHost;
 import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.delete.DeleteRequest;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.update.UpdateRequest;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
@@ -71,6 +77,60 @@ public class HotelIndexTest {
         boolean exists = client.indices().exists(request, RequestOptions.DEFAULT);
         // 3.输出
         System.out.println(exists);
+    }
+
+
+    @Test
+    public void testIndexDocument() throws IOException {
+        // 1.创建request对象
+        IndexRequest request = new IndexRequest("testclient").id("1");
+        // 2.准备JSON文档
+        request.source("{\"name\": \"jack\"}", XContentType.JSON);
+        // 3.发送请求
+        client.index(request, RequestOptions.DEFAULT);
+    }
+
+    @Test
+    public void testGetDocumentById() throws IOException {
+        // 1.创建request对象
+        GetRequest request = new GetRequest("testclient", "1");
+        // 2.发送请求，得到结果
+        GetResponse response = client.get(request, RequestOptions.DEFAULT);
+        // 3.解析结果
+        String json = response.getSourceAsString();
+
+        System.out.println(json);
+    }
+
+    @Test
+    public void testUpdateDocumentById() throws IOException {
+        // 1.创建request对象
+        UpdateRequest request = new UpdateRequest("testclient", "1");
+        // 2.准备参数
+        request.doc("name", "rose");    // 后面还有就继续加
+        // 3.更新文档
+        client.update(request, RequestOptions.DEFAULT);
+    }
+
+    @Test
+    public void testDeleteDocumentById() throws IOException {
+        // 1.创建request对象
+        DeleteRequest request = new DeleteRequest("testclient", "1");
+        // 2.发送请求
+        client.delete(request, RequestOptions.DEFAULT);
+    }
+
+    @Test
+    public void testBulk() throws IOException {
+        // 1.创建Bulk请求
+        BulkRequest request = new BulkRequest();
+        // 2.添加要批量提交的请求
+        request.add(new IndexRequest("testclient").id("1")
+                .source("{\"name\": \"jack\"}", XContentType.JSON));
+        request.add(new IndexRequest("testclient").id("2")
+                .source("{\"name\": \"rose\"}", XContentType.JSON));
+        // 3.发起bulk请求
+        client.bulk(request, RequestOptions.DEFAULT);
     }
 
 }
